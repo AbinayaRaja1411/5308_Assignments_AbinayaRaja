909be4f0c15fb406e1e7dc140c74e91bb135a208
Abinaya Raja <abinayaraja1411@gmail.com>
2019-06-14 03:18:23 -0300

    A3

--- A2/Q3/Student.java
+++ A2/Q3/Student.java
@@ -1,6 +1,3 @@
-import java.io.*;
-import java.util.Scanner;
-
 public class Student
 {
 	String bannerID;
@@ -34,38 +31,5 @@ public class Student
 	public void setEmail(String email)
 	{
 		this.email = email;
-	}
-
-	public void save()
-	{
-		try
-		{
-			PrintWriter writer = new PrintWriter("student.txt", "UTF-8");
-			writer.println(bannerID);
-			writer.println(firstName);
-			writer.println(lastName);
-			writer.println(email);
-			writer.close();
-		}
-		catch (Exception e)
-		{
-			System.out.println("I am a bad programmer that hid an exception.");
-		}
-	}
-
-	public void load()
-	{
-		try
-		{
-			Scanner in = new Scanner(new FileReader("student.txt"));
-			bannerID = in.next();
-			firstName = in.next();
-			lastName = in.next();
-			email = in.next();
-		}
-		catch (Exception e)
-		{
-			System.out.println("I am a bad programmer that hid an exception.");
-		}
 	}
 }

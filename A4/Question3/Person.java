18f9a84d1808d55dafa932ba8b8bc5fe2c972fe5
Abinaya Raja <abinayaraja1411@gmail.com>
2019-07-19 17:11:09 -0300

    Q3 issue fix

--- A4/Question3/Person.java
+++ A4/Question3/Person.java
@@ -1,11 +1,12 @@
 public class Person
 {
 	public String name;
-	public Address address = new Address("Rob street", "Rob city", "Rob province", "Rob postalcode");
+	public Address address;
 
 	public Person()
 	{
 		name = "Rob";
+		address = new Address("Rob street", "Rob city", "Rob province", "Rob postalcode");
 	}
 
 	public boolean isPersonRob()
